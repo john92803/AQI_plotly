49830590ae1632dd349a5743e42d69b9e1503853
john92803 <john92803@gmail.com>
2025-05-20 14:13:10 +0800

    add api comment

--- AQI_plotly.py
+++ AQI_plotly.py
@@ -4,8 +4,8 @@ import plotly.express as px
 import webbrowser
 import os
 
-# API 設定
-API_URL = "https://data.moenv.gov.tw/api/v2/aqx_p_432?api_key=9e565f9a-84dd-4e79-9097-d403cae1ea75&limit=1000&sort=ImportDate desc&format=JSON"
+# API 設定 放上自己要的json API
+# API_URL = "https://data.moenv.gov.tw/api/v2/aqx_p_432?api_key=9e565f9a-84dd-4e79-9097-d403cae1ea75&limit=1000&sort=ImportDate desc&format=JSON"
 
 # 取得資料
 response = requests.get(API_URL)
@@ -42,7 +42,7 @@ fig = px.scatter_mapbox(
 fig.update_layout(
     mapbox_style="carto-positron",
     title="台灣空氣品質指標（AQI）地圖",
-    margin={"r":0,"t":40,"l":0,"b":0}
+    margin={"r": 0, "t": 40, "l": 0, "b": 0}
 )
 
 filename = "aqi_map.html"
